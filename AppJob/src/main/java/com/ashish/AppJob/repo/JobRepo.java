dbfa13351d411a908c578a5f3d4aaeefebd78154
Ashish <ashishraj0609@gmail.com>
2025-09-08 19:09:21 +0530

    Job Repository

--- AppJob/src/main/java/com/ashish/AppJob/repo/JobRepo.java
+++ AppJob/src/main/java/com/ashish/AppJob/repo/JobRepo.java
@@ -1,6 +1,12 @@
 package com.ashish.AppJob.repo;
 
+import com.ashish.AppJob.entity.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface JobRepo extends JpaRepository<JobId, Integer> {
+import java.util.List;
+
+@Repository
+public interface JobRepo extends JpaRepository<JobEntity, Integer> {
+    List<JobEntity> findByPostProfileContainingIgnoreCaseOrPostDescContainingIgnoreCase(String postProfile, String postDesc);
 }
