cd05c6fb799a6a9f61d4f158abea9f252d99ec13
Ashish <ashishraj0609@gmail.com>
2025-09-18 23:56:27 +0530

    added oauth component

---
+++ AppJobUI/src/oAuth/OAuth2RedirectHandler.jsx
@@ -0,0 +1,12 @@
+import React ,{useEffect} from 'react' ;
+import { useSearchParams ,useNavigate } from 'react-router-dom';
+
+function OAuth2RedirectHandler() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default OAuth2RedirectHandler
