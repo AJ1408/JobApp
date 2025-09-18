9d10e0823ec8680806b967b92aa06177cce901d1
Ashish <ashishraj0609@gmail.com>
2025-09-18 23:54:57 +0530

    add oauth2 route

--- AppJobUI/src/App.jsx
+++ AppJobUI/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './Pages/Login'
 import SignUp from './Pages/SignUp'
 import DashBoard from './Pages/DashBoard'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import OAuth2RedirectHandler from './oAuth/OAuth2RedirectHandler';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/dashboard" element={<DashBoard />} />
+        <Route path ="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
+        <Route path="/student-dashboard" element={<DashBoard />} />
+        <Route path="/recruiter-dashboard" element={<DashBoard />} />
       </Routes>
     </Router>
     </>
