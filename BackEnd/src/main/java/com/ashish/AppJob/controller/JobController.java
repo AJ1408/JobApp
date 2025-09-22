0c74f66e709c0dd3269d7aef88b65d1d898143c0
Ashish <ashishraj0609@gmail.com>
2025-09-22 23:08:52 +0530

    Created Controller for Job

--- BackEnd/src/main/java/com/ashish/AppJob/controller/JobController.java
+++ BackEnd/src/main/java/com/ashish/AppJob/controller/JobController.java
@@ -3,60 +3,69 @@ package com.ashish.AppJob.controller;
 import com.ashish.AppJob.entity.JobEntity;
 import com.ashish.AppJob.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000",allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(origins = "http://localhost:5173",allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@RequestMapping("/api/jobs")
 public class JobController {
 
     @Autowired
     public JobService service;
 
-    @GetMapping("/")
-    public String Welcome(){
-        return "Welcome to Job Portal" ;
-    }
+// //   @GetMapping("/")
+//    public String Welcome(){
+//        return "Welcome to Job Portal" ;
+//    }
 
-    @GetMapping("/jobpost")
+    //ToGetAllJobs
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public List<JobEntity> getAllJobs() {
         return service.getAllJobs();
     }
-
-    @GetMapping("/jobpost/{postId}")
-    public JobEntity getJobById (@PathVariable Integer postId){
+    //GetJobById
+    @GetMapping("/{postId}")
+    @PreAuthorize("isAuthenticated()")
+    public JobEntity getJobById(@PathVariable Integer postId){
         return service.getJob(postId);
     }
-    @GetMapping("/jobpost/keyword/{keyword}")
-    public List<JobEntity> searchByKeyword(@PathVariable("keyword") String keyword) {
+    //SearchJob
+    @GetMapping("/search")
+    @PreAuthorize("isAuthenticated()")
+    public List<JobEntity> searchByKeyword(@RequestParam("keyword") String keyword) {
         return service.search(keyword);
     }
 
-    @PostMapping("/jobpost/")
+    @PostMapping()
+    @PreAuthorize("hasRole('RECRUITER')")
     public String addJob( @RequestBody JobEntity jobPost){
         service.addJob(jobPost);
         return "Job Added Successfully";
     }
 
-    @PutMapping("/jobpost")
-    public String updateJob( @RequestBody JobEntity jobPost){
+    @PutMapping("/{postId}")
+    @PreAuthorize("hasRole('RECRUITER')")
+    public String updateJob( @PathVariable Integer postId, @RequestBody JobEntity jobPost){
+        jobPost.setPostId(postId);
         service.updateJob(jobPost);
         return "Job Updated Successfully";
     }
 
-    @DeleteMapping("/jobpost/{postId}")
-    public String deleteJob(@PathVariable Integer postID) {
-        service.deleteJob(postID);
+    @DeleteMapping("/{postId}")
+    @PreAuthorize("hasRole('RECRUITER')")
+    public String deleteJob(@PathVariable Integer postId) {
+        service.deleteJob(postId);
         return "Job Deleted Successfully";
     }
 
-
-
-    @GetMapping("/home")
-    public String loadData() {
-        service.load();
-        return "Data Loaded Successfully";
-    }
+//    @GetMapping("/home")
+//    public String loadData() {
+//        service.load();
+//        return "Data Loaded Successfully";
+//    }
 
 }
