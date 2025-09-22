23c31bdbe63c08f02662b7ab3fe3561a0d3c17a8
Ashish <ashishraj0609@gmail.com>
2025-09-22 23:09:24 +0530

    Created Job Entity

--- BackEnd/src/main/java/com/ashish/AppJob/entity/JobEntity.java
+++ BackEnd/src/main/java/com/ashish/AppJob/entity/JobEntity.java
@@ -1,9 +1,6 @@
 package com.ashish.AppJob.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +19,7 @@ public class JobEntity {
     private String postProfile;
     private String postDesc;
     private Integer reqExperience;
+    @ElementCollection
     private List<String> postTechStack; // Store as a comma-separated string
 
 }
