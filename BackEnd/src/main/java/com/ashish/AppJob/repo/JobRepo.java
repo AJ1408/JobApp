63b34b12e7d93ce39814c832b71e22b8cf7f4339
Ashish <ashishraj0609@gmail.com>
2025-10-02 21:15:28 +0530

    updated job repo

--- BackEnd/src/main/java/com/ashish/AppJob/repo/JobRepo.java
+++ BackEnd/src/main/java/com/ashish/AppJob/repo/JobRepo.java
@@ -1,6 +1,7 @@
 package com.ashish.AppJob.repo;
 
 import com.ashish.AppJob.entity.JobEntity;
+import com.ashish.AppJob.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,9 @@ import java.util.List;
 @Repository
 public interface JobRepo extends JpaRepository<JobEntity, Integer> {
     List<JobEntity> findByPostProfileContainingIgnoreCaseOrPostDescContainingIgnoreCase(String postProfile, String postDesc);
+
+    // for finding job by recruiter ID
+    List<JobEntity> findByRecruiter_UserId(int userId);
+
+
 }
